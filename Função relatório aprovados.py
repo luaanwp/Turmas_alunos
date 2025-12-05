3c0d485e4a744b961bc68a15c859139ad4345b26
Luan Lucas <luanlucf@gmail.com>
2025-12-04 23:07:53 -0300

    Alterações e adicionado alunos reprovados (função)

--- Função relatório aprovados.py
+++ Função relatório aprovados.py
@@ -1,6 +1,15 @@
 def listar_aprovados(lista):
     print(f"\n--- Alunos Aprovados ---")
-    print(f"\n---- Média(7,0) ---")
+    print(f"\n---- Média (7,0) ---")
     for aluno in lista:
-        if aluno["notas"] >= 6:
-            print(aluno["nome"])
+        if aluno["notas"] >= 7:
+            print(f"{aluno['nome']} - Nota: {aluno['notas']}")
+    print("------------------------------------")
+def listar_reprovados(lista):
+    print(f"\n--- Alunos Reprovados ---")
+    print(f"\n--- Média (7,0) ---")
+    for aluno in lista:
+        if aluno ["notas"] <=6:
+            print(f"{aluno['nome']} - Nota: {aluno['notas']}")
+    print("------------------------------------")
+## agora so chamar a função.
